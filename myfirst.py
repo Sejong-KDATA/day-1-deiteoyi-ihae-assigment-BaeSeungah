e3ed674f795a5583c39b12a001fb09db9b56c95f
BaeSeungah <67894316+BaeSeungah@users.noreply.github.com>
2020-07-08 18:51:42 +0900

    데이터 청년 캠퍼스 1일차 실습
    데이터 청년 캠퍼스 1일차 실습 파일

---
+++ myfirst.py
@@ -0,0 +1,23 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+print("I Love pizza!")
+I Love pizza!
+>>> print("pizza"*5)
+pizzapizzapizzapizzapizza
+>>> print("yum"*3)
+yumyumyum
+>>> print("I'm full.")
+I'm full.
+>>> Professor = "David Embley"
+>>> print(Professor)
+David Embley
+>>> a = 7
+>>> b = 5
+>>> print(a+b)
+12
+>>> a = 7
+>>> b = 5
+>>> print("a+b ")
+a+b 
+>>> 
